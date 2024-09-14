f661878e7f682b0b80cf4bfebb345494e5c08ca9
Jhon Galvez <jhon.galvezchambi@gmail.com>
2024-09-14 01:41:34 -0500

    implementa la logica del algoritmo BFS

--- src/services/MinimumPath.py
+++ src/services/MinimumPath.py
@@ -1,6 +1,35 @@
+from collections import deque
+
 class MinimumPath:
     def __init__(self):
-        pass
+        self.graph = {
+            "A": ["B", "C"],
+            "B": ["A", "D", "E"],
+            "C": ["A", "F"],
+            "D": ["B"],
+            "E": ["B", "F"],
+            "F": ["C", "E"]
+        }
 
-    def bfs(self, src: str, dest: str) -> str:
-        return src + " " + dest
+    def bfs(self, src: str, dest: str) -> list:
+        if src not in self.graph or dest not in self.graph:
+            return [f"No se encontró la ruta entre {src} y {dest}"]
+        
+        queue = deque([[src]])
+        visited = set()
+
+        while queue:
+            path = queue.popleft()
+            node = path[-1]
+
+            if node == dest:
+                return path  # Esto es una lista de nodos
+            
+            if node not in visited:
+                visited.add(node)
+                for neighbor in self.graph[node]:
+                    if neighbor not in visited:
+                        queue.append(path + [neighbor])
+
+        return [f"No hay ruta disponible entre {src} y {dest}"]
+   
