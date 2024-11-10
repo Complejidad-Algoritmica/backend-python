02a3b8faa95ec1ba58d7d0ea654293e6c0710159
Arian Rodriguez <arianmrv12@gmail.com>
2024-11-10 18:33:24 -0500

    update: cors added

--- src/main.py
+++ src/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from .routes.Route import router
 
+#include cors
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(
-    title="Complejidad Algoritimica Backend",
+    title="Complejidad Algoritmica Backend",
     description="Endpoints to manage the minimum path"
 )
 
-
+# Add CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
 
 app.mount("/", app)
